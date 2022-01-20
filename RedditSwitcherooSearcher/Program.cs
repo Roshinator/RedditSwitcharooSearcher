caea7ad4af5f661ed9f67a6e531ee15bb7dc97b1
Roshinator <sevaliaroshan@gmail.com>
2022-01-20 00:00:29 -0800

    Add project files.

---
+++ RedditSwitcherooSearcher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+class Program
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    static async Task Main(string[] args)
+    {
+        await ProcessComments();
+    }
+
+    private static async Task ProcessComments()
+    {
+        client.DefaultRequestHeaders.Accept.Clear();
+        //client.DefaultRequestHeaders.Accept.Add() Add headers here
+        client.DefaultRequestHeaders.Add("User-Agent", "Reddit Switcheroo Searcher");
+
+        
+    }
+}
